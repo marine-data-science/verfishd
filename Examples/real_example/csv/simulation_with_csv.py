d8eb5c4e1811e2540347a66305d0a1bd6d149b5d
Jan Meischner <jan.meischner@googlemail.com>
2025-06-25 10:25:02 +0200

    add plotting to example

--- Examples/real_example/csv/simulation_with_csv.py
+++ Examples/real_example/csv/simulation_with_csv.py
@@ -1,14 +1,24 @@
+import matplotlib.pyplot as plt
 import os
+
 from Examples.real_example.example_factors import Oxygen, Temperature, TemperatureGradient, Light
 from verfishd import VerFishDModel, StimuliProfile, migration_speed_with_demographic_noise
-import matplotlib.pyplot as plt
 
-mig_speed = lambda x: x
 data_file = f"{os.path.dirname(__file__)}/aggregated_data_8A.csv"
 profile = StimuliProfile.read_from_tabular_file(data_file)
-model = VerFishDModel('Hol 8A', profile, migration_speed_with_demographic_noise, [Oxygen('oxygen', 0.52), Temperature('temperature', 0.14), TemperatureGradient('temperature_gradient', 0.14), Light('light', 0.2)])
+
+model = VerFishDModel(
+    'Hol 8A',
+    profile,
+    migration_speed_with_demographic_noise,
+    [
+        Oxygen('oxygen', 0.52),
+        Temperature('temperature', 0.14),
+        TemperatureGradient('temperature_gradient', 0.14),
+        Light('light', 0.2)]
+)
 
 model.simulate(800)
-model.plot()
 
+model.plot()
 plt.show()
